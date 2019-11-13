03775ea7b1de164589713d71fc96844538cc10de
Michal Nowak <michal.nowakb@gmail.com>
2019-11-13 12:19:18 +0000

    changes commited

--- src/main/java/com/sparta/mjn/services/RegistrationService.java
+++ src/main/java/com/sparta/mjn/services/RegistrationService.java
@@ -15,23 +15,12 @@ public class RegistrationService {
 
     Map<String,String> userBase;
 
-    public static User createUser(String user){
-        String userName = null;
-        String firstName = null;
-        String lastName = null;
-        String password = null;
-
-        User createdUser = new User(userName,firstName,lastName,password);
-        return createdUser;
-    }
-
-    public String registerUser(String user){
-        User registeredUser = createUser(user);
-        userBase = new HashMap<>();
-        String key = registeredUser.getUserName();
-        userBase.put(key, String.valueOf(registeredUser));
 
 
+    public String registerUser(User user){
+        if(user.getUserName().equals("eXo")){
+            return "UserNameBeenUsed";
+        } else
         return "welcomeNewUser";
     }
 }
